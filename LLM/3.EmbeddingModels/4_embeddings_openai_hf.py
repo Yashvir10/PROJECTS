aa43480ad5a52832970a25a5c4e54826a7bf1057
Yashvir Bhardwaj <yashvirbhardwaj10@gmail.com>
2026-08-13 22:31:47 +0530

    Add files via upload

---
+++ LLM/3.EmbeddingModels/4_embeddings_openai_hf.py
@@ -0,0 +1,28 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+
+embedding=OpenAIEmbeddings(model="text-embedding-3-large",dimension=32)
+
+documents=[
+    "delhi is the capital of India.",
+    "New Delhi is the capital of India.",   
+    "kolkata is the capital of West Bengal.",
+    "paris is the capital of France.",
+]
+
+query = "tell me about delhi"
+
+doc_embeddings = embedding.embed_documents(documents)
+query_embedding = embedding.embed_query(query)
+
+
+scores = cosine_similarity([query_embedding], doc_embeddings)[0]
+
+index,score = sorted(list(enumerate(scores)), key=lambda x: x[1])
+
+print(documents[index])
+print("similarity score:",score)
